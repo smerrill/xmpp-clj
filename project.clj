4c1e7acaa6ff0e9a8f5f7be459ed792249a2b43c
Steven Merrill <steven.merrill@gmail.com>
2012-08-05 22:08:01 -0400

    Update smack version, grab smackx.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject xmpp-clj "0.2.1"
   :description "A lightweight clojure wrapper around the smack jabber/XMPP library"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [jivesoftware/smack "3.1.0"]])
+                 [org.igniterealtime.smack/smack "3.2.1"]
+                 [org.igniterealtime.smack/smackx "3.2.1"]])
